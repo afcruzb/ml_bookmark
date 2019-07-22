35d27f9831dfd06c7062fe724cb938f6f7a9c2bb
Andres <afcruzb@unal.edu.co>
2019-07-21 22:54:04 -0500

    initial commit

---
+++ base.js
@@ -0,0 +1,7 @@
+javascript: (function() {
+    s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://rawgit.com/davatron5000/fitWeird/master/fitWeird.js?v=';
+    document.body.appendChild(s);
+})();
+
